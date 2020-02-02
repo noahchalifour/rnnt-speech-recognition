6cf4cf04f6ec00d4d1dfd8c201536bb8de2bdfd7
Noah Chalifour <chalifournoah@gmail.com>
2020-02-01 21:39:53 -0500

    Fixed ffmpeg preprocessing error

--- preprocess_common_voice.py
+++ preprocess_common_voice.py
@@ -6,9 +6,32 @@ import os
 
 def mp3_to_wav(filepath):
 
-    audio_segment = AudioSegment.from_mp3(filepath)
-    audio_segment.export('{}.wav'.format(filepath[:-4]), format='wav')
+    try:
+        audio_segment = AudioSegment.from_mp3(filepath)
+        audio_segment.export('{}.wav'.format(filepath[:-4]), format='wav')
+    except Exception:
+        pass
+
     os.remove(filepath)
+
+
+def remove_missing(data_dir, fname):
+
+    clips_dir = os.path.join(data_dir, 'clips')
+
+    old_filepath = os.path.join(data_dir, '{}.tsv'.format(fname))
+    new_filepath = os.path.join(data_dir, '{}-tmp.tsv'.format(fname))
+
+    with open(old_filepath, 'r') as old_f:
+        with open(new_filepath, 'w') as new_f:
+            new_f.write(next(old_f))
+            for line in old_f:
+                audio_fn = line.split('\t')[1][:-4] + '.wav'
+                if os.path.exists(os.path.join(clips_dir, audio_fn)):
+                    new_f.write(line)
+
+    os.remove(old_filepath)
+    os.rename(new_filepath, old_filepath)
 
 
 def main(args):
@@ -25,6 +48,13 @@ def main(args):
 
         clip_fp = os.path.join(clips_dir, clip)
         mp3_to_wav(clip_fp)
+
+    print('Removing missing files...')
+
+    tsv_files = ['dev', 'invalidated', 'other', 'test', 'train', 'validated']
+
+    for _file in tsv_files:
+        remove_missing(args.data_dir, _file)
 
     print('Done.')
 
