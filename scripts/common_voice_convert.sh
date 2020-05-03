50690c77351d081150270032b1d1cc2fe3ae1255
Shaun <sman1@users.noreply.github.com>
2020-05-03 19:26:36 -0400

    Multi-thread conversion
    The original script does not do the job efficiently. I added in modifications to allow the conversion to run concurrently on multiple threads. A second command-line argument now allows the user to specify a thread count, with the default being single-threaded.

--- scripts/common_voice_convert.sh
+++ scripts/common_voice_convert.sh
@@ -5,16 +5,22 @@ IFS=$'\n'
 
 FORMAT=.mp3
 DATA_DIR="$1"
+N=${2:-1}
 
 mkdir -p $DATA_DIR
 
 FILES=$(ls "$DATA_DIR" | grep $FORMAT)
 
+thread () {
+    local FILE_N=$1
+    FILENAME="${FILE_N:0:${#FILE_N}-4}"
+    ffmpeg -i $DATA_DIR/$FILE_N -acodec pcm_s16le -ac 1 -ar 16000 $DATA_DIR/$FILENAME.wav
+    rm $DATA_DIR/$FILE_N
+}
 for FILE in $FILES
 do
-    FILENAME="${FILE:0:${#FILE}-4}"
-    ffmpeg -i $DATA_DIR/$FILE -acodec pcm_s16le -ac 1 -ar 16000 $DATA_DIR/$FILENAME.wav
-    rm $DATA_DIR/$FILE
+   ((i=i%N)); ((i++==0)) && wait
+   thread "$FILE" & 
 done
 
 IFS="$OIFS"
